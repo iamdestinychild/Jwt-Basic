5de869f669ee3d422482604733e6ec1bfcf90829
Destiny <omoruyidestiny30@gmail.com>
2023-03-23 05:45:14 +0100

    resolved error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require("http-status-codes");
-const { CustomError } = require("../errors/errors");
+const CustomError = require("../errors/errors");
 
 const errorHandler = (err, req, res, next) => {
   if (err instanceof CustomError) {
